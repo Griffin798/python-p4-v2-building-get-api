c660a9d8a1abd022a800396f3dc070b0356d908e
Griffin798 <griffo@DESKTOP-QM8LH4N.>
2024-07-06 11:49:47 +0300

    Complete Lab

--- server/app.py
+++ server/app.py
@@ -1,14 +1,13 @@
-# server/app.py
+#!/usr/bin/env python3
 
-from flask import Flask, jsonify, make_response
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, make_response, jsonify
 from flask_migrate import Migrate
 
-from models import db, User, Review, Game
+from models import db, Bakery, BakedGood
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
 migrate = Migrate(app, db)
@@ -17,46 +16,34 @@ db.init_app(app)
 
 @app.route('/')
 def index():
-    return "Index for Game/Review/User API"
+    return '<h1>Bakery GET API</h1>'
 
-# start building your API here
-@app.route('/games')
-def games():
+@app.route('/bakeries')
+def bakeries():
+    bakeries = [bakery.to_dict() for bakery in Bakery.query.all()]
+    return make_response(  bakeries,   200  )
 
-    games = [game.to_dict() for game in Game.query.all()]
+@app.route('/bakeries/<int:id>')
+def bakery_by_id(id):
 
-    response = make_response(
-        games,
-        200
-    )
+    bakery = Bakery.query.filter_by(id=id).first()
+    bakery_serialized = bakery.to_dict()
+    return make_response ( bakery_serialized, 200  )
 
-    return response
+@app.route('/baked_goods/by_price')
+def baked_goods_by_price():
+    baked_goods_by_price = BakedGood.query.order_by(BakedGood.price.desc()).all()
+    baked_goods_by_price_serialized = [
+        bg.to_dict() for bg in baked_goods_by_price
+    ]
+    return make_response( baked_goods_by_price_serialized, 200  )
+   
 
-@app.route('/games/<int:id>')
-def game_by_id(id):
-    game = Game.query.filter(Game.id == id).first()
-
-    game_dict = game.to_dict()
-
-    response = make_response(
-        game_dict,
-        200
-    )
-
-    return response
-
-@app.route('/games/users/<int:id>')
-def game_users_by_id(id):
-    game = Game.query.filter(Game.id == id).first()
-
-    # use association proxy to get users for a game
-    users = [user.to_dict(rules=("-reviews",)) for user in game.users]
-    response = make_response(
-        users,
-        200
-    )
-
-    return response
+@app.route('/baked_goods/most_expensive')
+def most_expensive_baked_good():
+    most_expensive = BakedGood.query.order_by(BakedGood.price.desc()).limit(1).first()
+    most_expensive_serialized = most_expensive.to_dict()
+    return make_response( most_expensive_serialized,   200  )
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
